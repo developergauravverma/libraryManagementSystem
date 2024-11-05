d6eed2845fc59a3157e1e9f36a462650d7afc885
Gaurav Verma <153429649+developergauravverma@users.noreply.github.com>
2024-11-06 00:39:05 +0530

    contact us is complete.

--- frontend/src/Features/Landing/Components/ContactUs/ContactUs.tsx
+++ frontend/src/Features/Landing/Components/ContactUs/ContactUs.tsx
@@ -1,9 +1,23 @@
+import { FC } from "react";
 import Styles from "./Styles.module.css";
 
-const {} = Styles;
+const { contactUs, contactUsDivider } = Styles;
 
-const ContactUs = () => {
-  return <div></div>;
+const ContactUs: FC = () => {
+  return (
+    <div className={contactUs}>
+      <h3>Contact Us</h3>
+      <h4>Address</h4>
+      <p>Modinagar</p>
+      <p>Agarsen Park</p>
+      <div className={contactUsDivider}></div>
+      <h4>Phone Number</h4>
+      <p>+91-8791804230</p>
+      <div className={contactUsDivider}></div>
+      <h4>Email Address</h4>
+      <p>developergauravverma@gmail.com</p>
+    </div>
+  );
 };
 
 export default ContactUs;
--- frontend/src/Features/Landing/index.ts
+++ frontend/src/Features/Landing/index.ts
@@ -2,5 +2,6 @@ import BookOfTheWeek from "./Components/BookOfTheWeek/BookOfTheWeek";
 import UpComingEvents from "./Components/UpComingEvents/UpComingEvents";
 import LibraryCard from "./Components/LibraryCard/LibraryCard";
 import LibraryHours from "./Components/LibraryHours/LibraryHours";
+import ContactUs from "./Components/ContactUs/ContactUs";
 
-export { BookOfTheWeek, UpComingEvents, LibraryCard, LibraryHours };
+export { BookOfTheWeek, UpComingEvents, LibraryCard, LibraryHours, ContactUs };
